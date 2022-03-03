ce90d5ad4d9e72b17a38de4609e05caff1ad44de
tamrakarsweta220 <tamrakar0220@gmail.com>
2022-03-03 18:26:37 -0500

    finished building basic user stories

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -6,10 +6,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG="MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -30,15 +33,28 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init{
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie){
             tvTitle.text =  movie.title
             tvOverview.text = movie.overview
             Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+        }
+
+        override fun onClick(v: View?) {
+            //1. Get notified on the particular movie which was clicked
+            val movie = movies[adapterPosition]
+            //2. Use the intent system to navigate to the new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 }
